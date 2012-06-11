2287429c5d4e87691052ecc01fd5e82bfa7705a9
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2012-06-11 12:31:37 +0300

    Partial fix for #16

--- bad-code/bad-code.c
+++ bad-code/bad-code.c
@@ -4,29 +4,29 @@
 int check_if_prime(int n)
 {
 int i;
-  for(i=2;i<n/2;i++)
-	if(n%i==0)
+  for(i = 2;i < n/2;i++)
+	if(n % i == 0)
 		return 0;
 return 1;
 }
 
 int get_sum(int n)
-{ int i, sum=0;
-	for (i=1; i <=n; i++)
-		sum=sum+i;
+{ int i, sum = 0;
+	for (i = 1; i <= n; i++)
+		sum += i;
 return sum;
 }
 
 int main(){
-  int extra; char *len="prime";
+  int extra; char *len = "prime";
 int num;
      int is_prime,i;
-  for(i=0;i<10;i++){printf("Introduceti numar:");
+  for(i = 0;i < 10;i++){printf("Introduceti numar:");
 	       scanf("%d", &num);
-      is_prime=check_if_prime(num);
+      is_prime = check_if_prime(num);
         if(is_prime)
             printf("is %s\n",len); else printf("is not %s\n",len);
-     extra=get_sum(num);
+     extra = get_sum(num);
    printf("sum (1..%d) is %d\n",num,extra);
  }
 
